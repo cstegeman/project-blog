0b36d97d08cdd64f6e63b557ac0dfdb22fbaca97
cstegeman <c.stegeman@gmail.com>
2024-11-11 20:42:12 +0100

    Exercise 3: Adding metadata

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -5,10 +5,26 @@ import BlogHero from '@/components/BlogHero';
 import styles from './postSlug.module.css';
 import {loadBlogPost} from "@/helpers/file-helpers";
 import {MDXRemote} from "next-mdx-remote/rsc";
+import {BLOG_TITLE} from "@/constants";
+
+const getBlogPost = React.cache(async (slug) => {
+    return await loadBlogPost(slug);
+});
+
+export async function generateMetadata({params}) {
+    const {postSlug} = params;
+    const post = await getBlogPost(postSlug);
+    const {frontmatter: {title, abstract}} = post;
+
+    return {
+        title: `${title} • ${BLOG_TITLE}`,
+        description: abstract,
+    }
+}
 
 async function BlogPost({params}) {
     const {postSlug} = params;
-    const post = await loadBlogPost(postSlug);
+    const post = await getBlogPost(postSlug);
     const {frontmatter, content} = post;
 
     return (
@@ -18,7 +34,7 @@ async function BlogPost({params}) {
                 publishedOn={frontmatter.publishedOn}
             />
             <div className={styles.page}>
-                {content && <MDXRemote source={content} />}
+                {content && <MDXRemote source={content}/>}
             </div>
         </article>
     );
